5ae530033b7fe39dc1f83972bb07e09d829ac5fc
Matteo Conti <contimatteo1505@gmail.com>
2024-02-04 22:24:17 +0100

    WIP

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 pip install -U pip wheel setuptools
-pip install torch==2.0.1 torchvision==0.15.2 xformers==0.0.20 --index-url https://download.pytorch.org/whl/cu118
+# pip install torch==2.0.1 torchvision==0.15.2 xformers==0.0.20 --index-url https://download.pytorch.org/whl/cu118
+pip install torch==2.0.1 torchvision==0.15.2 xformers --index-url https://download.pytorch.org/whl/cu118
 pip install -r custom_requirements.txt
 pip install submodules/diff-gaussian-rasterization/
 pip install submodules/simple-knn/
