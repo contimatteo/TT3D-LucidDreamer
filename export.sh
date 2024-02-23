2188044aafd41c191944b911e9a47ba4313e3c21
Matteo Conti <contimatteo1505@gmail.com>
2024-02-23 18:48:34 +0100

    WIP

--- export.sh
+++ export.sh
@@ -8,7 +8,7 @@ ENV="report"
 PROMPT="n100"
 EXPERIMENT_PREFIX="t3bench/single"
 
-ROOT_DIR="/media/data2/mconti/TT3D"
+ROOT_DIR="/media/data3/mconti/TT3D"
 OUT_DIR="${ROOT_DIR}/outputs/${ENV}/${EXPERIMENT_PREFIX}/${PROMPT}"
 
 
@@ -18,4 +18,9 @@ OUT_DIR="${ROOT_DIR}/outputs/${ENV}/${EXPERIMENT_PREFIX}/${PROMPT}"
 # CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_export.py \
 python3 tt3d_export.py \
   --source-path "${OUT_DIR}/LucidDreamer/" \
+  --skip-existing
+
+
+python3 tt3d_export.py \
+  --source-path "${OUT_DIR}/LucidDreamer-nopriors/" \
   --skip-existing
